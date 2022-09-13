94b73efb06e169d537360def8716711d09a955e2
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2022-09-13 12:46:01 -0400

    Genesis

---
+++ 8.5.c
@@ -0,0 +1,11 @@
+/* Teste de caracteres. Escreva um programa que receba
+um caractere do teclado e teste-o com cada uma das fun-
+ções na biblioteca de tratamento de caractere. O progra-
+ma deverá imprimir o valor retornado por cada função. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+}
