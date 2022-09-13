8172b4920116c5422a8a299ad682e8ff1b2d86fe
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2022-09-13 16:12:23 -0400

    Atualização

--- 8.6.c
+++ 8.6.c
@@ -6,7 +6,7 @@ maiúsculas e minúsculas. */
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 
 void imprimeStringMaiuscula(char s[]) {
     int i = 0;
@@ -26,7 +26,7 @@ void imprimeStringMinuscula(char s[]) {
 
 int main(void) {
     char *s = calloc(sizeof(char), 100);
-    scanf("%[^'\n']%*c", s);
+    scanf("%[^\n]%*c", s);
     printf("String original: %s\n", s);
     int tam = sizeof(s) / sizeof(s[0]);
     imprimeStringMaiuscula(s);
